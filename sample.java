d2eab5b235646466a5e491c6d024722be51652dd
rajeshkalyan <48218135+rajeshkalyan@users.noreply.github.com>
2019-03-04 22:55:47 +0530

    added a variable

--- sample.java
+++ sample.java
@@ -2,6 +2,7 @@ public class sample{
  private eno Integer;
  private ename String;
  private salary Double;
+ private country String;
  
  public void calculateEmployeeSalary(){
       system.out.println("Hello am in method calculateEmployeeSalary()");
