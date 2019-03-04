43384388a244e6c4202179c64a1e007c5c7d6ac3
rajeshkalyan <48218135+rajeshkalyan@users.noreply.github.com>
2019-03-04 22:30:03 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,9 @@
+public class sample{
+ private eno Integer;
+ private ename String;
+ private salary Double;
+ 
+ public void calculateEmployeeSalary(){
+      system.out.println("Hello am in method calculateEmployeeSalary()");
+ }
+}
